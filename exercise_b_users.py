32e5bf5601e8b24a0b02411a454308a3ab8ab817
emma-dougherty <doughertyemma@hotmail.com>
2022-03-29 17:32:12 +0100

    exb q7-10 completed

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -72,7 +72,26 @@ for number in Avrils_even_lottery_numbers:
       print(number)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"] += [7]
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
 # 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append(
+  {"name": "fluffy",
+    "species": "dog"})
+
 # 10. Add another person to the users dictionary
-print(users)
+users["Michael"] = ({
+  "Michael": {
+    "twitter": "bestboss",
+    "lottery_numbers": [2, 4, 7, 8, 24, 15],
+    "home_town": "Dundee",
+    "pets": [
+    {
+      "name": "biggles",
+      "species": "fish"}
+    ]
+    }
+  }
+)
+
